c15734cca16468ee6fb6a1ffef8d75630c013552
Brian Lee <email": null>
2016-06-17 16:24:18 -0400

    Got to reesult page

--- app/controllers/round_controller.rb
+++ app/controllers/round_controller.rb
@@ -6,8 +6,18 @@ get '/rounds/:id' do
   if current_round.guesses.length == 0
     @card = current_round.cards.shuffle.first
     session[:card_id] = @card.id
+    erb :'rounds/show'
+  elsif current_round.guesses.where(correct: true).length == current_round.cards.length
+    erb :'rounds/result'
+  else
+    round_card_id = shuffle
+    while Guess.where(round_id: current_round.id, card_id: round_card_id, correct: true).length > 0
+      round_card_id = shuffle
+    end
+    @card = Card.find(round_card_id)
+    erb :'rounds/show'
   end
-  erb :'rounds/show'
+
 end
 
 post '/rounds/:id' do
@@ -18,5 +28,8 @@ post '/rounds/:id' do
   else
     input = false
   end
-  Guess.create!(round: @round, card: @card, user: User.find(session[:user_id]), correct: input)
+  Guess.create!(round: @round, card: @card, user: current_user, correct: input)
+
+  redirect "/rounds/#{current_round.id}"
 end
+
--- app/helpers/deck_helper.rb
+++ app/helpers/deck_helper.rb
@@ -6,3 +6,11 @@ def current_round
   Round.find(session[:round_id])
 end
 
+def current_user
+  User.find(1)
+end
+
+def shuffle
+  current_round.cards.shuffle.first.id
+  # current_round.cards.
+end
