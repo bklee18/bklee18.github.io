3856437275e65d4cdcff579d83d12daea607a2af
Marco Manomat <email": null>
2016-06-17 14:34:36 -0400

    add user authorization

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,2 +1,53 @@
 get '/' do
+  redirect "/login"
+end
+
+get '/login' do
+  if login?
+    redirect '/home'
+  else
+    erb :'marco/login'
+  end
+end
+
+post  '/login' do
+ @user = User.find_by(username: params[:username])
+  if @user
+    if @user.authenticate(params[:password])
+      session[:user_id]=@user.id
+      redirect '/'
+    else
+      @errors = "Incorrect password."
+    end
+  else
+    @errors = 'No such username.'
+  end
+  erb :'marco/login'
+end
+
+get '/new' do
+  if login?
+    erb :'marco/home'
+  else
+    erb :'marco/new'
+  end
+end
+
+post '/new' do
+  user = User.new(username:params[:username], email:params[:email], password:params[:password])
+  if user.save
+    redirect '/'
+  else
+    @errors = user.errors.full_messages
+    erb :'marco/new'
+  end
+end
+
+get '/home' do
+  erb :'marco/home'
+end
+
+get '/logout' do
+ session.delete(:user_id)
+ redirect '/'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
   has_many :rounds
   has_many :guesses
+
+  validates :username, :password, :email, presence: true
+  validates :username, uniqueness: true
+  has_secure_password
+
 end
